a11bacf589e399ecf6d182ea881f6fa9aa45f6b6
trndud153 <trndud153@naver.com>
2023-12-01 00:04:34 +0900

    23

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const startServer = async () => {
   const app = createApp();
 
   const PORT = process.env.PORT || 3001;
-
+  app.set('host', '0.0.0.0');
   app.listen(PORT, '0.0.0.0', async () => {
-    await appDataSource
-      .initialize()
-      .then(() => {
-        console.log('Data Source has been initialized!');
-      })
-      .catch((error) => {
-        console.error('Error during Data Source initialization', error);
-      });
+    // await appDataSource
+    //   .initialize()
+    //   .then(() => {
+    //     console.log('Data Source has been initialized!');
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error during Data Source initialization', error);
+    //   });
     console.log(`Listening to request on 0.0.0.0:${PORT}`);
   });
 };
